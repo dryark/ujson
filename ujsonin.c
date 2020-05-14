5704d9f3bad83b1d5686cdc4df45af6616348eb4
nanoscopic <github@codechild.com>
2020-05-13 23:42:27 -0700

    Split code into header and main. Fix node_hash__delete
    Put the demo code into main.c; and split ujsonin.c up properly into a .c and a .h file.
    Fixed node_hash__delete to properly also delete the associated string_tree that it allocates.

--- ujsonin.c
+++ ujsonin.c
@@ -3,47 +3,7 @@
 #include"red_black_tree.h"
 #include"string-tree.h"
 
-#include"red_black_tree.c"
-#include"string-tree.c"
-
-typedef struct jnode_s jnode;
-
-#define NODEBASE char type; jnode *parent;
-// type 1=hash, 2=str
-
-#define SAFE(x) if(pos>=len) { endstate=x; goto Done; }
-#define SAFEGET(x) if(pos>=len) { endstate=x; goto Done; } let=data[pos++];
-#define SPACES for( int j=0;j<depth;j++ ) printf("  ");
-
-struct jnode_s { NODEBASE };
-
-typedef struct node_hash_s { NODEBASE
-    string_tree *tree;
-} node_hash;
-
-typedef struct node_str_s { NODEBASE
-    char *str;
-    int len;
-} node_str;
-
-typedef struct node_arr_s { NODEBASE
-    jnode *head;
-    jnode *tail;
-    int count;
-} node_arr;
-
-typedef struct parser_state_s {
-    int state;
-} parser_state;
-
-node_hash *parse( char *data, int len, parser_state *beginState, int *err );
-jnode *node_hash__get( node_hash *self, char *key, int keyLen );
-void jnode__dump( jnode *self, int depth );
-char *slurp_file( char *filename, int *outlen );
-void ujsonin_init();
-void jnode__dump_env( jnode *self );
-void node_hash__dump_to_makefile( node_hash *self, char *prefix );
-void node_hash__del( node_hash *self );
+#include"ujsonin.h"
 
 node_hash *parse_with_default( char *file, char *def, char **d1, char **d2 ) {
     int flen;
@@ -69,96 +29,6 @@ node_hash *parse_with_default( char *file, char *def, char **d1, char **d2 ) {
     *d1 = fdata;
     if( ddata ) *d2 = ddata;
     return both;
-}
-
-int main( int argc, char *argv[] ) {
-    ujsonin_init();
-    
-    if( argc == 1 ) {
-        printf("Usage");
-        exit(0);
-    }
-    string_tree *args = string_tree__new();
-    char *cmd = argv[1];
-    for( int i=2;i<argc;i++ ) {
-        char *key = argv[i];
-        if( key[0] == '-' ) {
-            key++;
-            char *val = argv[++i];
-            char extra = 0;
-            char *vals[7] = {0,0,0,0,0,0,0};
-            for( int j=i+1;j<argc;j++ ) {
-                char *another = argv[j];
-                if( another[0] == '-' ) break;
-                extra++;
-                vals[extra] = another;
-            }
-            if( extra ) {
-                vals[0] = val;
-                string_tree__store_len( args, key, strlen( key ), (void *) vals, 2 );
-                i += extra;
-            }
-            else {
-                string_tree__store_len( args, key, strlen( key ), (void *) val, 1 );
-            }
-        }
-    }
-    
-    char type;
-    if( !strncmp(cmd,"makefile",8) ) {
-        char *file = string_tree__get_len( args, "file", 4, &type );
-        char *defaults = string_tree__get_len( args, "defaults", 8, &type );
-        char *prefix1 = string_tree__get_len( args, "prefix", 6, &type );
-        
-        char prefix[100];
-        if( prefix1 ) sprintf(prefix,"%s_",prefix1);
-        char *d1, *d2;
-        node_hash__dump_to_makefile( parse_with_default(file,defaults, &d1, &d2 ), prefix1 ? prefix : 0 );
-        exit(0);
-    }
-    if( !strncmp(cmd,"get",3) ) {
-        char *file = string_tree__get_len( args, "file", 4, &type );
-        if( !file ) {
-            fprintf(stderr,"-file must be specified\n");
-            exit(1);
-        }
-        char *defaults = string_tree__get_len( args, "defaults", 8, &type );
-        char *d1,*d2;
-        jnode *cur = (jnode *) parse_with_default( file, defaults, &d1, &d2 );
-        
-        void *pathV = string_tree__get_len( args, "path", 4, &type );
-        if( !pathV ) {
-            fprintf(stderr,"-path must be specified\n");
-            exit(1);
-        }
-        char *onepart[2] = {0,0};
-        char **parts;
-        if( type == 1 ) {
-            onepart[0] = (char *) pathV;
-            parts = onepart;
-        }
-        if( type == 2 ) parts = ( char ** ) pathV;
-        
-        for( int i=0;i<6;i++ ) {
-            char *part = parts[i];
-            if( !part ) break;
-            node_hash *curhash = ( node_hash * ) cur;
-            cur = node_hash__get( curhash, part, strlen( part ) );
-        }
-        jnode__dump_env( cur );
-        exit(0);
-    }
-    if( !strncmp(cmd,"test",4) ) {
-        int len;
-        char *data = slurp_file( "test.json", &len );
-        int err;
-        node_hash *root = parse( data, len, NULL, &err );
-        jnode__dump( (jnode *) root, 0 );
-        node_hash__del( root );
-        exit(0);   
-    }
-    fprintf(stderr,"Unknown command '%s'\n", cmd );
-    return 1;
 }
 
 char *slurp_file( char *filename, int *outlen ) { 
@@ -224,15 +94,17 @@ void node_hash__dump( node_hash *self, int depth ) {
     SPACES printf("}\n");
 }
 
-void node_hash__del( node_hash *self ) {
+void node_hash__delete( node_hash *self ) {
     xjr_key_arr *keys = string_tree__getkeys( self->tree );
     for( int i=0;i<keys->count;i++ ) {
         char *key = keys->items[i];
         int len = keys->sizes[i];
         jnode *sub = node_hash__get( self, key, len );
-        if( sub->type == 1 ) node_hash__del( sub );
+        if( sub->type == 1 ) node_hash__delete( (node_hash *) sub );
         else free( sub );
     }
+    xjr_key_arr__delete( keys );
+    string_tree__delete( self->tree );
     free( self );
 }
 
